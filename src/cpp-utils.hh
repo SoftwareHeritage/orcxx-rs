0be1b0342ee32e6dbfc2c94fe38914c0d2cb5e61
vlorentz <vlorentz@softwareheritage.org>
2023-06-27 09:40:26 +0000

    Add minimal support for reading binaries and strings

--- src/cpp-utils.hh
+++ src/cpp-utils.hh
@@ -1,5 +1,8 @@
 #include <memory>
 
+#include <orc/MemoryPool.hh>
+#include <orc/Vector.hh>
+
 namespace orcxx_rs {
 
     // Constructs a C++ object using this trick:
@@ -18,4 +21,51 @@ namespace orcxx_rs {
     {
       return obj.numElements;
     }
+
+
+    template<typename T, typename Ret>
+    Ret
+    get_length(T &obj)
+    {
+      return obj.length;
+    }
+
+    template<typename T, typename Ret>
+    Ret
+    get_data(T &obj)
+    {
+      return obj.data;
+    }
+
+    template<typename T, typename Ret>
+    Ret
+    get_fields(T &obj)
+    {
+      return obj.fields;
+    }
+
+    template<typename T, typename Ret>
+    Ret
+    try_into(T obj) {
+      return dynamic_cast<Ret>(obj);
+    }
+
+    template<typename T, typename Ret>
+    std::unique_ptr<Ret>
+    ptr_try_into(std::unique_ptr<T> obj) {
+      std::unique_ptr<Ret> p(dynamic_cast<Ret*>(obj.get()));
+      return p;
+    }
+
+    typedef orc::DataBuffer<char*> StringDataBuffer;
+    typedef orc::DataBuffer<int64_t> Int64DataBuffer;
+    typedef orc::ColumnVectorBatch* ColumnVectorBatchPtr;
+
+    template<typename T, typename Ret>
+    Ret
+    into(T obj)
+    {
+      return obj;
+    }
 }
+
--- src/lib.rs
+++ src/lib.rs
@@ -21,12 +21,28 @@ mod ffi {
         fn construct() -> UniquePtr<RowReaderOptions>;
     }
 
+    #[namespace = "orcxx_rs"]
+    unsafe extern "C++" {
+        type Int64DataBuffer;
+
+        fn data(&self) -> *const i64;
+    }
+
+    #[namespace = "orcxx_rs"]
+    unsafe extern "C++" {
+        type StringDataBuffer;
+
+        fn data(&self) -> *const *mut c_char;
+    }
+
     #[namespace = "orc"]
     unsafe extern "C++" {
         type InputStream;
         type ReaderOptions;
         type RowReaderOptions;
         type ColumnVectorBatch;
+        type StringVectorBatch;
+        type StructVectorBatch;
 
         fn readLocalFile(path: &CxxString) -> Result<UniquePtr<InputStream>>;
     }
@@ -54,13 +70,36 @@ mod ffi {
 
     #[namespace = "orcxx_rs"]
     unsafe extern "C++" {
+        type ColumnVectorBatchPtr;
+
+        #[rust_name = "ColumnVectorBatchPtr_make_ptr"]
+        fn into(batch_ptr: &ColumnVectorBatchPtr) -> *const ColumnVectorBatch;
+
         fn get_numElements(vectorBatch: &ColumnVectorBatch) -> u64;
+
+        //#[rust_name = "try_into_StringColumnBatch"]
+        //fn ptr_try_into(vectorBatch: UniquePtr<ColumnVectorBatch>) -> Result<UniquePtr<StringVectorBatch>>;
+        #[rust_name = "try_into_StringColumnBatch"]
+        fn try_into(vectorBatch: &ColumnVectorBatch) -> Result<&StringVectorBatch>;
+        #[rust_name = "StringVectorBatch_get_data"]
+        fn get_data(vectorBatch: &StringVectorBatch) -> &StringDataBuffer;
+        #[rust_name = "StringVectorBatch_get_length"]
+        fn get_length(vectorBatch: &StringVectorBatch) -> &Int64DataBuffer;
+
+        #[rust_name = "try_into_StructColumnBatch"]
+        fn try_into(vectorBatch: &ColumnVectorBatch) -> Result<&StructVectorBatch>;
+        #[rust_name = "StructVectorBatch_get_fields"]
+        fn get_fields(vectorBatch: &StructVectorBatch) -> &CxxVector<ColumnVectorBatchPtr>;
+
     }
 }
 
 #[cfg(test)]
 mod tests {
     use cxx::let_cxx_string;
+
+    use std::convert::TryInto;
+    use std::ffi::CStr;
 
     use super::*;
 
@@ -84,12 +123,48 @@ mod tests {
         let mut batch = row_reader.createRowBatch(1024);
 
         let mut total_elements = 0;
+        let mut all_strings = Vec::new();
         while row_reader.pin_mut().next(batch.pin_mut()) {
             for _ in 0..ffi::get_numElements(&*batch) {
                 total_elements += 1;
             }
+
+            let struct_data = ffi::StructVectorBatch_get_fields(
+                &*ffi::try_into_StructColumnBatch(&*batch)
+                    .expect("could not cast ColumnVectorBatch to StructDataBuffer"),
+            );
+
+            for sub_batch in struct_data {
+                match ffi::try_into_StringColumnBatch(unsafe {
+                    &*ffi::ColumnVectorBatchPtr_make_ptr(sub_batch)
+                }) {
+                    Ok(sub_batch) => {
+                        let data = ffi::StringVectorBatch_get_data(sub_batch);
+                        let lengths = ffi::StringVectorBatch_get_length(sub_batch);
+                        let num_elements: usize = ffi::get_numElements(&*batch)
+                            .try_into()
+                            .expect("could not convert u64 to usize");
+                        unsafe {
+                            for (&s, &length) in std::iter::zip(
+                                std::slice::from_raw_parts(data.data(), num_elements),
+                                std::slice::from_raw_parts(lengths.data(), num_elements),
+                            ) {
+                                all_strings.push(std::str::from_utf8(std::slice::from_raw_parts(
+                                    s as *const u8,
+                                    length.try_into().expect("could not convert u64 to usize"),
+                                )))
+                            }
+                        }
+                    }
+                    Err(e) => println!("failed to cast to StringDataBuffer: {:?}", e),
+                }
+            }
         }
 
         assert_eq!(total_elements, 2);
+        assert_eq!(
+            all_strings,
+            vec![Ok("\0\u{1}\u{2}\u{3}\u{4}"), Ok(""), Ok("hi"), Ok("bye")]
+        );
     }
 }
